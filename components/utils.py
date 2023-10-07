fcb6be11859377fea5fbf25b07fccc0458041190
nguyennghia0902 <nguyennghia0902@gmail.com>
2023-10-07 09:14:39 +0700

    Update the encryption and decryption function on the same file

--- components/utils.py
+++ components/utils.py
@@ -29,69 +29,90 @@ def forget_old_widgets(frame: tk.Frame):
             if name != 'loginframe':
                 widget.destroy()
 
-def encrypt_file(file_in, password):        
-    fileIn = open(file_in, 'rb')
+def read_size_bs_bytes(size, data):
+    bs = AES.block_size
+    chunk_size = size * bs
+    offset = 0
+    while offset < len(data):
+        chunk = data[offset:offset + chunk_size]
+        offset += chunk_size
+        yield chunk
+
+def encrypt_file(file_in, password): 
+    file = open(file_in, 'rb')
+    data = file.read()
+    file.close()
+
+    name = str(file_in)
+    name += ".enc"
+    os.rename(file_in, name)
+
+    file = open(name, 'wb')
+    bs = AES.block_size 
+    pass_hashed = hash_password(password)
+    Ksession = get_digest(pass_hashed)  #os.urandom(bs)
+    cipher = AES.new(Ksession, AES.MODE_CBC)
+    finished = False
+    
+    file.write(cipher.iv)
+
+    file.write(bytes(pass_hashed, 'utf-8'))
+
+    while not finished:
+        for chunk in read_size_bs_bytes(1024, data):
+            if len(chunk) == 0 or len(chunk) % bs != 0:   
+                padding_length = (bs - len(chunk) % bs) or bs
+                chunk += str.encode(padding_length * chr(padding_length))
+                finished = True
+            file.write(cipher.encrypt(chunk))   
+        pass         
+    file.close()
+    return True
+    
+def decrypt_file(file_in, passinput):  
+    file = open(file_in, 'rb')
     file_name = file_in.split('/')[-1]
     name = file_name
     addr = file_in.split('/')[:-1]
     file_out = '/'.join(addr)
     if '.' in file_name:
-        name = file_name.split('.')[0]
-    file_name = file_name.replace(name, name + '_encrypted')
-    file_out += '/' + file_name
-    fileOut = open(file_out, 'wb+')
-    bs = AES.block_size 
-    pass_hashed = hash_password(password)
-    Ksession = get_digest(pass_hashed)#os.urandom(bs)
-    cipher = AES.new(Ksession, AES.MODE_CBC)
-    finished = False
-
-    fileOut.write(cipher.iv)
-
-    fileOut.write(bytes(pass_hashed, 'utf-8'))
-
-    while not finished:
-        chunk = fileIn.read(1024 * bs) 
-        if len(chunk) == 0 or len(chunk) % bs != 0:   
-            padding_length = (bs - len(chunk) % bs) or bs
-            chunk += str.encode(padding_length * chr(padding_length))
-            finished = True
-        fileOut.write(cipher.encrypt(chunk))                 
-    
-    fileIn.close()
-    fileOut.close()
-    return True
-
-def decrypt_file(file_in, passinput):                     #5.4
-    fileIn = open(file_in, 'rb')
-    matches = re.findall(r'/\w+',file_in)
-    file_name = matches[-1]
-    file_name = file_name[1:]
-    if file_name.find('_encrypted') != -1:
-        file_name.replace('_encrypted','')
-    file_out = file_in.replace(file_name, file_name + '_decrypted')
-    fileOut = open(file_out, 'wb+')
-    bs = AES.block_size #16 bytes
-    
-    iv = fileIn.read(bs)
-
-    cont = fileIn.read(289-33)
-    check = hash_password(passinput)
-    pass_hashed = cont[:len(check)].decode()
-
+        full = file_name.split('.')
+        name = full[0]
+        exte = full[-1]
+    if exte == "enc":    
+        filename = str(file_in)
+        filename = filename[:-4]
+        print(filename)
+        bs = AES.block_size 
+        iv = file.read(bs)
+        check = hash_password(passinput)
+        passhase = file.read(len(check))
+        data = file.read()
+        file.close()
+        os.rename(file_in, filename)
+    else:
+        messagebox.showerror(title='LỖI', message='Không phải file mã hóa!\nVui lòng chọn lại!')
+        return False
+    file = open(filename, 'wb+')
+    pass_hashed = passhase[:len(check)].decode()
     if validate_password(passinput, pass_hashed):
       key = get_digest(pass_hashed)
       cipher = AES.new(key, AES.MODE_CBC, iv)
       next_chunk = ''
       finished = False
+      offset = 0
+      chunk_size = 1024 * bs
       while not finished:
-          chunk, next_chunk = next_chunk, cipher.decrypt(fileIn.read(1024 * bs))
+          chunk, next_chunk = next_chunk, cipher.decrypt(data[offset:offset + chunk_size])
+          offset += chunk_size
           if len(next_chunk) == 0:
             padding_length = chunk[-1]
             chunk = chunk[: - padding_length]  # type: ignore
             finished = True 
-          fileOut.write(bytes(x for x in chunk))   # type: ignore
+          file.write(bytes(x for x in chunk))   # type: ignore
       return True
     messagebox.showerror(title='LỖI', message='Sai  Mật khẩu!\nVui lòng nhập lại!')
-    return False
+    file.close()
+    return False                 
+    
     
